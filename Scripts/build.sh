6e41a223b30e95b91546c6087f8e419fe56ae9d2
John Coates <john@johncoates.dev>
2020-07-01 10:42:43 -0500

    Use org.swift.libswift on iOS versions lower than 12.2

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -6,9 +6,9 @@ projectDirectory="$scriptsDirectory/../"
 
 pushd "$projectDirectory"
     xcodebuild -scheme flexdecrypt -sdk iphoneos -configuration Release -derivedDataPath ./derived
-    binDirectory="$projectDirectory/Package/usr/bin"
+    binDirectory="./Package/usr/bin"
     mkdir -p "$binDirectory"
     cp ./derived/Build/Products/Release-iphoneos/flexdecrypt.app/flexdecrypt "$binDirectory/"
     find ./Package -name ".DS_Store" -depth -exec rm {} \;
-    dpkg-deb -b -Zgzip "$projectDirectory/Package" flexdecrypt-1.0.deb
+    dpkg-deb -b -Zgzip ./Package flexdecrypt.deb
 popd
