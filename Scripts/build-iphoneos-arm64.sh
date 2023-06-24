50e5548c858237eb0d0c759d18dcc61aaa04d4a5
Lightmann <lightmanns11@gmail.com>
2023-06-24 10:23:41 -0400

    Fixes

--- Scripts/build-iphoneos-arm64.sh
+++ Scripts/build-iphoneos-arm64.sh
@@ -8,10 +8,10 @@ projectDirectory="$scriptsDirectory/../"
 
 pushd "$projectDirectory"
     sed -i '' 's/IPHONEOS_DEPLOYMENT_TARGET = 11.0;/IPHONEOS_DEPLOYMENT_TARGET = 15.0;/g' flexdecrypt.xcodeproj/project.pbxproj
-    xcodebuild -scheme flexdecrypt -sdk iphoneos -configuration Release -derivedDataPath ./derived
-    binDirectory="./Package/var/jb/usr/bin"
+    xcodebuild -scheme flexdecrypt -sdk iphoneos -configuration Release -derivedDataPath ./derived-rootless
+    binDirectory="./Package-rootless/var/jb/usr/bin"
     mkdir -p "$binDirectory"
-    cp ./derived/Build/Products/Release-iphoneos/flexdecrypt.app/flexdecrypt "$binDirectory/"
-    find ./Package -name ".DS_Store" -depth -exec rm {} \;
+    cp ./derived-rootless/Build/Products/Release-iphoneos/flexdecrypt.app/flexdecrypt "$binDirectory/"
+    find ./Package-rootless -name ".DS_Store" -depth -exec rm {} \;
     dpkg-deb -b -Zgzip ./Package-rootless flexdecrypt-rootless.deb
 popd
